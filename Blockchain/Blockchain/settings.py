ac93bd213af39b14020264a6e7cd452f1ffd7726
18Pixels(I) Pvt. Ltd <rk468335@gmail.com>
2021-09-30 14:00:45 +0530

    .env file ceated for blockchain project

--- Blockchain/Blockchain/settings.py
+++ Blockchain/Blockchain/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# dotenv.read_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
